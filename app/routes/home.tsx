66a9cc727e737208a3d39a808c21bbaf0b2cfac1
bakshi-philips <saurav.bakshi@philips.com>
2025-01-21 17:51:19 +0530

    create new records and show selected link

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <p id="index-page">
+            This is a demo for React Router.
+            <br />
+            Check out{" "}
+            <a href="https://reactrouter.com">
+                the docs at react-router.com
+            </a>
+            .
+        </p>
+    );
+}
